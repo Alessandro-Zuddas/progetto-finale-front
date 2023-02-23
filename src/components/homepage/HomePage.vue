4fcd3a37f07c66c2dee19874e2b7c7f7ced3fc01
Alessandro-Zuddas <alessandro.zuddas1999@gmail.com>
2023-02-23 18:01:05 +0100

    Implementazione carosello (da aggiustare scroll)

--- src/components/homepage/HomePage.vue
+++ src/components/homepage/HomePage.vue
@@ -1,6 +1,7 @@
 <script>
 import HomeHeader from "./partials/HomeHeader.vue";
 import HomeHero from "./partials/HomeHero.vue";
+import HomeCarousel from "./partials/HomeCarousel.vue";
 import HomeFooter from "./partials/HomeFooter.vue";
 
 export default {
@@ -9,15 +10,21 @@ export default {
   components: {
     HomeHeader,
     HomeHero,
+    HomeCarousel,
     HomeFooter,
-  },
+},
 };
 </script>
 
 <template>
   <HomeHeader />
   <HomeHero />
+  <HomeCarousel/>
   <HomeFooter />
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+
+
+
+</style>

c483a789e0a099885b1b1e73d8e345f301b514df
SimoVisc <simonaviscardivs@gmail.com>
2023-02-23 11:01:17 +0100

    Aggiunge colonne e immagini footer

---
+++ src/components/homepage/partials/HomeFooter.vue
@@ -0,0 +1,62 @@
+<script>
+export default {
+  name: "HomeFooter",
+};
+</script>
+
+<template>
+  <section>
+    <div class="container text-center">
+      <div class="row">
+        <div class="col-3">
+          <h3>Scopri Deliveroo</h3>
+          <ul>
+            <li><a href="#">Investitori</a></li>
+            <li><a href="#">Chi siamo</a></li>
+            <li><a href="#">Ristoranti</a></li>
+            <li><a href="#">Altro</a></li>
+            <li><a href="#">Pressroom</a></li>
+            <li><a href="#">Programmazione</a></li>
+            <li><a href="#">Design</a></li>
+            <li><a href="#">Lavora con noi</a></li>
+            <li><a href="#">Diventa nostro partner</a></li>
+          </ul>
+        </div>
+        <div class="col-3">
+          <h3>Note legali</h3>
+          <ul>
+            <li><a href="#">Termini & Condizioni</a></li>
+            <li><a href="#">Informativa sulla privacy</a></li>
+            <li><a href="#">Cookies</a></li>
+          </ul>
+        </div>
+        <div class="col-3">
+          <h3>Aiuto</h3>
+          <ul>
+            <li><a href="#">Contatti</a></li>
+            <li><a href="#">FAQ</a></li>
+            <li><a href="#">Tipi di cucina</a></li>
+          </ul>
+        </div>
+        <div class="col-3">
+          <h3>Porta Deliveroo con te</h3>
+          <ul>
+            <li>
+              <a href="#"><img src="assets/img/badge-app-store" alt="" /></a>
+            </li>
+            <li>
+              <a href="#"><img src="assets/img/badge-app-store" alt="" /></a>
+            </li>
+            <li><a href="#">Tipi di cucina</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+section {
+  background-color: #2e3333;
+}
+</style>

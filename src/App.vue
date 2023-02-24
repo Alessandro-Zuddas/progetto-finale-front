7327858b027f47896a5ccd59588aaa61ed074d22
Alessandro-Zuddas <alessandro.zuddas1999@gmail.com>
2023-02-24 16:22:50 +0100

    Aggiunge vue routing e store.js

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,36 @@
 <script>
+
+import axios from "axios";
 import HomePage from "./components/homepage/HomePage.vue";
+import { store } from "../src/store";
+
+
 export default {
   components: {
     HomePage,
   },
+  data(){
+    return {
+      store,
+    }
+  },
+  created(){
+    axios.get("http://127.0.0.1:8000/api/typologies")
+    .then(result => {
+
+      this.store.typologies = result.data;
+      console.log(this.store.typologies);
+
+    });
+
+    axios.get("http://127.0.0.1:8000/api/companies")
+    .then(result => {
+
+      this.store.companies = result.data;
+      console.log(this.store.companies);
+
+    });
+  }
 };
 </script>
 
--- src/router.js
+++ src/router.js
@@ -1,9 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+import HomePage from "../src/components/homepage/HomePage.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
+
+    {
+      path: '/',
+      name: 'homepage',
+      component: HomePage
+    },
+
   ],
 });
 

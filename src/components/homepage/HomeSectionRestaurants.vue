e3072bb80b143186798c3c9e763ced4d98b33f7b
Alessandro-Zuddas <alessandro.zuddas1999@gmail.com>
2023-03-01 16:53:15 +0100

    Aggiunge grafica checkbox

--- src/components/homepage/HomeSectionRestaurants.vue
+++ src/components/homepage/HomeSectionRestaurants.vue
@@ -52,14 +52,16 @@
 <template>
     <div class="ms-container container shadow py-5">
         <div class="row">
-            <div class="ms-col-aside col-3 ">
-                <div class="ms-title-container ms-3 pb-4">
-                    <h2 class="ms-2">Categorie:</h2>
+            <div class="ms-col-aside col-3 d-flex align-items-center">
+                <div class="ms-title-container my-4 pb-4">
+                    <h2 class="ms-category-text">Categorie:</h2>
                 </div>
-                <label :for=" typology.slug " class="ms-5 text-secondary ms-check-restaurant" v-for="typology in this.store.typologies">
-                    <input type="checkbox" :value=" typology.slug " :id=" typology.slug " class="me-1" v-model="this.selectedTypes">
-                    {{ typology.name }}
-                </label>
+                <div :class="`ms-check-background ms-background-${typology.id}`" v-for="typology in this.store.typologies">
+                        <label :for=" typology.slug " class="ms-5 ms-check-restaurant">
+                            <input type="checkbox" :value=" typology.slug " :id=" typology.slug " class="me-1" v-model="this.selectedTypes" style="zoom:1.2;">
+                            <h3 class="ms-category-title">{{ typology.name }}</h3>
+                        </label>
+                </div>
             </div>
             
             <div class="ms-col-big col-9">
@@ -94,6 +96,7 @@
 <style lang="scss" scoped>
 
     .ms-container{
+        position: relative;
         margin-top: 100px;
         padding: .625rem 10px;
         background: rgba(255, 255, 255, 0.7);
@@ -143,6 +146,12 @@
         margin-top: .3125rem;
     }
 
+    .ms-category-text{
+        position: absolute;
+        top: 5%;
+        left: 7%;
+    }
+
     .ms-button{
        background-color:  rgba(23, 196, 185, 1);
        color: #fff;
@@ -150,12 +159,68 @@
     }
 
     .ms-check-restaurant{
+        font-size: 19px;
         padding: 10px;
-        border-bottom: 1px solid white;
     }
 
     input[type=checkbox] {
         accent-color: rgba(23, 196, 185, 1);
+    }
+
+    .ms-check-background{
+        position: relative;
+        width: 100%;
+        padding: 10px 10px;
+        margin-bottom: 10px;
+        border-radius: 10px;
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center;
+        filter: grayscale(0.4);
+        cursor: pointer;
+    }
+
+    .ms-category-title{
+        position: absolute;
+        top: 23%;
+        left: 32%;
+        color: white;
+    }
+
+    .ms-background-1{
+        background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfY0rG3h36m60KnjBCl7OmJD25SfJxNWeuNA&usqp=CAU);
+    }
+
+    .ms-background-2{
+        background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhODdAUH8YKjh48hY_9HxHdi_6PcDXWR_L2A&usqp=CAU);
+    }
+
+    .ms-background-3{
+        background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBDOxqL6PMsnyreIoD7CKqaJ9Ci7iY5bYvjA&usqp=CAU);
+    }
+
+    .ms-background-4{
+        background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsYb7Nft0RzuGJVp3ycycja0pMYATL3japiQ&usqp=CAU);
+    }
+
+    .ms-background-5{
+        background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6eliCoMB-1bPmMatM06o9K1EBwkm7NCRmHQ&usqp=CAU);
+    }
+
+    .ms-background-6{
+        background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0GbZWT5MwgKVSiCT-NeH9nRj523xb8xewWQ&usqp=CAU);
+    }
+
+    .ms-background-7{
+        background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjcChp6A54gDCRtJ7mLgibHBpQh3wtpWgt1A&usqp=CAU);
+    }
+
+    .ms-background-8{
+        background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSF04ibBToBQwlrlaVgf7VSP6hVuFxdBAlhVg&usqp=CAU);
+    }
+
+    .ms-background-9{
+        background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDPIlOyya6y47R46rYXuIrzDhfHJk1HtneQg&usqp=CAU);
     }
 
 </style>
